525078dc2837e7300da0a8d8f80e9cd7c3ce3e10
lusbnz <vietdq.b21cn788@stu.ptit.edu.vn>
2024-10-30 21:25:49 +0700

    update

--- hooks/useSocket.js
+++ hooks/useSocket.js
@@ -5,16 +5,16 @@ const useSocket = (url, token) => {
   const [response, setResponse] = useState(null);
 
   useEffect(() => {
-    if (url && !url.includes("undefined")) {
+    if (url && !url.includes("undefined") && !url.includes("null")) {
       socketRef.current = new WebSocket(`${url}?token=${token}`);
 
       socketRef.current.onmessage = (event) => {
         setResponse(JSON.parse(event.data));
       };
 
-      // return () => {
-      //   socketRef.current.close();
-      // };
+      return () => {
+        socketRef.current.close();
+      };
     }
   }, [url, token]);
 
