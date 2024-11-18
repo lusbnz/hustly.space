d2cdd3aff944c0539fc1ecd387da69975092c921
lusbnz <vietdq.b21cn788@stu.ptit.edu.vn>
2024-11-18 21:54:07 +0700

    update

--- components/layout/ModalRejectConfirm.js
+++ components/layout/ModalRejectConfirm.js
@@ -24,7 +24,7 @@ const ModalRejectConfirm = ({ isOpen, toggleOpenModal, handleConfirm }) => {
             Are you sure you want to reject this user?{" "}
           </span>
           <span className="font-[400] text-[14px] text-gray-400">
-            If you reject this person, they won't be able to reach out to you
+            If you reject this person, they wont be able to reach out to you
             again, unless you reach out to them first. Are you sure you want to
             reject them? They might be sad 😞
           </span>
