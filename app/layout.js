07655aefea23fcec5da26d1274219a84102e51b2
lusbnz <vietdq.b21cn788@stu.ptit.edu.vn>
2024-12-18 01:11:03 +0700

    update

--- app/layout.js
+++ app/layout.js
@@ -4,18 +4,21 @@ import { Provider } from "react-redux";
 import "./globals.css";
 import store from "@/store/store";
 
-// export const metadata = {
-//   title: 'hustly.space',
-//   icons: {
-//     icon: '/logo-icon.svg',
-//     apple: '/logo-icon.svg',
-//   },
-// };
+const metadata = {
+  title: "hustly.space",
+  icons: {
+    icon: "/logo-icon.svg",
+    apple: "/logo-icon.svg",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <>
       <html lang="en">
+        <head>
+          <link rel="icon" href="/logo-icon.svg" type="image/svg" />
+        </head>
         <body>
           <Provider store={store}>{children}</Provider>
         </body>
