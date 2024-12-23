8b3b51727612b3dc5572d7b15b24a33b817a5683
lusbnz <vietdq.b21cn788@stu.ptit.edu.vn>
2024-12-23 23:12:36 +0700

    update

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,21 @@ export default function RootLayout({ children }) {
         <head>
           <title>hustly.space</title>
           <link rel="icon" href="/logo-icon.svg" type="image/svg" />
+          <meta name="description" content="hustly.space" />
+          <meta property="og:title" content="hustly.space" />
+          <meta property="og:description" content="hustly.space" />
+          <meta property="og:image" content="/logo-icon.svg" />
+          <meta property="og:type" content="website" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <script type="application/ld+json">
+            {JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              url: "https://hustly.space",
+              logo: "https://hustly.space/logo-icon.svg",
+              name: "hustly.space",
+            })}
+          </script>
         </head>
         <body>
           <Provider store={store}>{children}</Provider>
